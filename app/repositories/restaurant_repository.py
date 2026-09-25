1da4140c5e59af37beac9e8c32f10d944ec61a3f
Zuhaer Aziz <zuhaeraziz@gmail.com>
2026-09-25 13:06:17 -0700

    Added Restaurant Repository Layer

---
+++ app/repositories/restaurant_repository.py
@@ -0,0 +1,13 @@
+class RestaurantRepository:
+
+    def __init__(self, filepath: str = "data/restaurants.json"):
+        self.filepath = Path(filepath)
+
+    async def get_restaurants(self):
+        try:
+            with open(self.filepath, "r", encoding="utf-8") as file:
+                contents = json.load(file)
+                return contents
+        except FileNotFoundError:
+            raise FileNotFoundError(f"File {filepath} not found.")
+        return []
